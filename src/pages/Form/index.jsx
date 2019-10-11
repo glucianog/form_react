ebfaf2d07ef826fd9d3441f7ec747c007f2ae74e
glucianog <glgomes@sga.pucminas.br>
2019-10-11 06:17:55 -0300

    initial changes

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 import MaterialUiForm from '../../components/Form'
 
 export default function Form() {
-
     return (
         <>
             <MaterialUiForm />            
